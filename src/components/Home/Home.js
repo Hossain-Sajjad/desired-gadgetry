714f41c54712d3684e8f875f7693733a78dbddb2
Hossain-Sajjad <sunshinems58.com>
2022-04-05 19:44:02 +0600

    added show all reviews button

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import useReviews from '../../hooks/useReviews';
 import Review from '../Review/Review';
 import './Home.css'
@@ -6,13 +7,19 @@ import './Home.css'
 const Home = () => {
     const [reviews, setReviews] = useReviews();
 
+    const navigate = useNavigate();
+
+    const showAllReviews = () => {
+        navigate('/reviews');
+    }
+
     return (
         <div>
             <div className='md:flex justify-center'>
                 <div className='my-auto'>
                     <h1>Your Desire Device</h1>
                     <h3>Sony Alpha 7</h3>
-                    <p>The cameras are renowned for their reliability and quality</p>
+                    <p>Sony has some of the best mirrorless camera systems on the market. The cameras are renowned for their reliability and quality</p>
                 </div>
                 <div className='flex justify-end items-center'>
                     <img className='w-2/4' src="https://hips.hearstapps.com/amv-prod-gp.s3.amazonaws.com/gearpatrol/wp-content/uploads/2019/05/Complte-Sony-Camera-Buying-Guide-gear-patrol-ambiance-2.jpg?resize=768:*" alt="" />
@@ -28,7 +35,7 @@ const Home = () => {
                         ></Review>)
                     }
                 </div>
-                <button className='btn'>Show All</button>
+                <button onClick={showAllReviews} className='btn'>Show All</button>
             </div>
         </div>
     );
